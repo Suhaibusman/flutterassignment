ba1ee3e4abfe51cedd16ee3960bb638ae4684357
Suhaibusman <suhaibusman54@gmail.com>
2023-06-28 01:57:00 -0700

    assignment2

--- assignment3.dart
+++ assignment3.dart
@@ -24,6 +24,28 @@ for (var num in numbers) {
 print(evennumbers);
 print(oddnumber);
 
+//q#2
+//Write a program that prints the Fibonacci sequence up to a given
+// number using a for loop.
+// Example:
+// Input: 10
+// Output: 0 1 1 2 3 5 8
 
+  int n = 10;
+
+  // Initialize the first two Fibonacci numbers
+  int f1 = 0;
+  int f2 = 1;
+
+  // Print the Fibonacci sequence up to n
+  for (int i = 3; i < n; i++) {
+    // Print the current Fibonacci number
+    print(f1);
+
+    // Calculate the next Fibonacci number
+    int temp = f1;
+    f1 = f2;
+    f2 = temp + f2;
+  }
 
 }
