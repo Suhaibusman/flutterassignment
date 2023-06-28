1e0f96332a4f2133e18d1d2bd53b2842bb0c61b5
Suhaibusman <suhaibusman54@gmail.com>
2023-06-28 08:11:23 -0700

    assignment3

--- assignment3.dart
+++ assignment3.dart
@@ -221,5 +221,32 @@ for(int j=1;j<=i;j++){
 numb += j.toString();
 }
 print(numb);
+
 }
+
+
+
+
+//q#13
+
+// Write a program to make such a pattern like a right angle triangle with
+// a number which will repeat a number in a row.
+// The pattern like :
+// 1
+// 22
+// 333
+// 4444
+
+
+
+for(int i=1;i<=rows;i++){
+var numb = "";
+for(int j=1;j<=i;j++){
+numb += i.toString();
+}
+print(numb);
+
+}
+
+
 }
