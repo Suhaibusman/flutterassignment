dc76f3041b80907461ce32d1a042d1cb08032fb3
Suhaibusman <suhaibusman54@gmail.com>
2023-06-28 01:42:03 -0700

    assignment3

--- assignment3.dart
+++ assignment3.dart
@@ -1,5 +1,29 @@
+
+//MUHAMMAD SUHAIB USMAN
+//ID:- 127101
+
 void main(){
+//question 1
+//Write a program that takes a list of numbers as input and prints the
+// even numbers in the list using a for loop.
+// Example:
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Output: 2 4 6 8 10
+
+List numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+List evennumbers =[];
+List oddnumber =[];
+for (var num in numbers) {
+  if(num % 2 ==0){
+    evennumbers.add(num);
+  }else{
+    oddnumber.add(num);
+  }
+}
+
+print(evennumbers);
+print(oddnumber);
 
 
-  
+
 }
