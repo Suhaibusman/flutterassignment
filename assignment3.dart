57a9e7eab369add01250296f4b9cecc0538a28de
Suhaibusman <suhaibusman54@gmail.com>
2023-06-28 02:59:27 -0700

    asignment3

--- assignment3.dart
+++ assignment3.dart
@@ -159,7 +159,28 @@ if(inpString == revString){
   print("$inpString is a palindrome");
 }else{
   print("$inpString is not a palindrome");
+
+
+
 }
+
+
+
+//q#1o
+// Write a program to display the cube of the number up to an integer.
+// Test Data :
+// Input number of terms : 5
+// Expected Output :
+// Number is : 1 and cube of the 1 is :1
+// Number is : 2 and cube of the 2 is :8
+// Number is : 3 and cube of the 3 is :27
+// Number is : 4 and cube of the 4 is :64
+// Number is : 5 and cube of the 5 is :125
+//  int cubenumb =5;
+ for(int i=1;i<=5;i++){
+      print("Number is : $i and cube of the $i is :${i*i*i}");
+ }
+
 }
 
 
