96ef82addb3380102af4e74781c4757a0849f973
Suhaibusman <suhaibusman54@gmail.com>
2023-06-28 08:08:41 -0700

    assignment2

--- assignment3.dart
+++ assignment3.dart
@@ -206,7 +206,20 @@ for(int j=1;j<=i;j++){
    print(stars);
 }
 
+//q#12
+// Write a program to display a pattern like a right angle triangle with a
+// number using loop.
+// The pattern like :
+// 1
+// 12
+// 123
+// 1234
 
-
-
+for(int i=1;i<=rows;i++){
+var numb = "";
+for(int j=1;j<=i;j++){
+numb += j.toString();
+}
+print(numb);
+}
 }
